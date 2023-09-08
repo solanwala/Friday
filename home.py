575cdab6097df4f7d2b66e59ee6329cd960452c2
ankush <ankusht8@gmail.com>
2023-09-08 13:42:58 +0530

    this

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+from streamlit_option_menu import option_menu
+
+def dashboard():
+   st.title("This is dashboard")
+
+df = pd.DataFrame(
+   np.random.randn(50, 20),
+   columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
