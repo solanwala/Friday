6edba4e215bd5982d2cb6997c9d6fcaa89b71671
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-09 13:04:36 +0530

    kjgk

--- home.py
+++ home.py
@@ -4,10 +4,16 @@ import numpy as np
 from streamlit_option_menu import option_menu
 
 def dashboard():
-   st.title("This is dashblsddhlkshlgkoard")
+   selected = option_menu(None, ["Create Reminder", "View Reminders", "Chat", "Logout"], icons = ["📝", "📋", "💬", "🔓"] )
 
-   df = pd.DataFrame(
-      np.random.randn(50, 20), 
-      columns=('col %d' % i for i in range(20)))
-
-   st.dataframe(df)  # Same as st.write(df)
+   if selected == "Create Reminder":
+      create_rem()
+   elif selected == "View Reminders":
+      view_rem()
+   elif selected == "Chat":
+      chatting()
+   elif selected == "Logout":
+      st.session_state["users"] == "Guest"
+      st.experimental_rerun()
+   else:
+      st.write("No option selected")
