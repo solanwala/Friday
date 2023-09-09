cb1973abee8e77a04881d95297773bb01c77d8df
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-09 12:56:44 +0530

    this

--- home.py
+++ home.py
@@ -4,10 +4,10 @@ import numpy as np
 from streamlit_option_menu import option_menu
 
 def dashboard():
-   st.title("This is dashboard")
+   st.title("This is dashblsddhlkshlgkoard")
 
-df = pd.DataFrame(
-   np.random.randn(50, 20), 
-   columns=('col %d' % i for i in range(20)))
+   df = pd.DataFrame(
+      np.random.randn(50, 20), 
+      columns=('col %d' % i for i in range(20)))
 
-st.dataframe(df)  # Same as st.write(df)
+   st.dataframe(df)  # Same as st.write(df)
