2a993e5141f5c4386c113313ddb11f5f682541b5
solanwala <142795290+solanwala@users.noreply.github.com>
2023-09-09 16:05:18 +0530

    mbnb

--- auth.py
+++ auth.py
@@ -34,7 +34,11 @@ def Signup():
             else:
                 "Password do not match"
 
-    
+
+def test():
+    value = st.text_input("Value")
+    with open("data.txt", "w") as file:
+        file.write(value)
 
 def Login():
     username = st.text_input("username")
@@ -55,12 +59,14 @@ def main():
     
     if st.session_state["users"] == "Guest":
         with st.sidebar:
-            selected = option_menu("Menu", ["Login", "Signup"], icons = ["house", "person"])
+            selected = option_menu("Menu", ["Login", "Signup", "testing"], icons = ["house", "person"])
         if selected == "Login":
             Login()
 
         elif selected == "Signup":
             Signup()
+        elif selected == "testing":
+            test()
     elif st.session_state["users"] == "User":
         dashboard()
 
